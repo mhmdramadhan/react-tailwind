58fb72a2306fd485c212e2a521589ece2af840a9
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2023-06-22 16:20:14 +0700

    push data

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,7 @@
 const Loading = () => {
   return (
     <div className="m-2 h-full">
-      <div className="px-3 py-1 text-xs font-medium grid leading-none text-center text-white bg-blue-200 rounded-full animate-pulse dark:bg-gray-900  h-8">
-        <p className="place-self-center">loading...</p>
-      </div>
+      <h1>Loading...</h1>
     </div>
   );
 };
--- src/page/home/Index.jsx
+++ src/page/home/Index.jsx
@@ -3,10 +3,12 @@ import gambarJumbotron from '../../assets/greeting.svg';
 import PortofolioCard from '../../components/PortfolioCard';
 import DataContext from '../../context/DataContext';
 import Loading from '../../components/Loading';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Home = () => {
   const { portofolio } = useContext(DataContext);
-
   const [berita, setBerita] = useState([]);
   const [isLoading, setIsloading] = useState(false);
 
@@ -36,43 +38,56 @@ const Home = () => {
     }
   };
 
+  const settings = {
+    dots: true,
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false
+  };
+
   useEffect(() => {
     getBeritaKotaBogor();
   }, []);
 
   return (
     <Fragment>
-      <div className="mx-auto p-20 grid grid-cols-1 xl:grid-cols-2 h-screen">
-        <div className="grid place-content-center">
-          <h1 className="text-3xl text-gray-700 font-bold bg-gray-300 shadow rounded-md px-2">
-            Hallo! Selamat datang 👋
-          </h1>
-          <p className="text-2xl text-gray-700 font-thin px-2">
-            Mari cari tahu lebih banyak
-          </p>
+      <section>
+        <div className="mx-auto p-20 grid grid-cols-1 xl:grid-cols-2 h-screen">
+          <div className="grid place-content-center">
+            <h1 className="text-3xl text-gray-700 font-bold bg-gray-300 shadow rounded-md px-2">
+              Hallo! Selamat datang 👋
+            </h1>
+            <p className="text-2xl text-gray-700 font-thin px-2">
+              Mari cari tahu lebih banyak
+            </p>
+          </div>
+          <div className="grid xl:place-content-center place-content-start">
+            <img src={gambarJumbotron} className="w-full" />
+          </div>
         </div>
-        <div className="grid xl:place-content-center place-content-start">
-          <img src={gambarJumbotron} className="w-full" />
+      </section>
+      <section>
+        <div className="container bg-gray-800 mx-auto p-8 rounded-xl -mb-10 relative z-10">
+          <h2 className="text-3xl font-bold text-white">Portofolio Saya</h2>
         </div>
-      </div>
-      <div className="container bg-gray-800 mx-auto p-8 border shadow rounded-xl -mb-10 relative z-10">
-        <h2 className="text-3xl font-bold text-white">Portofolio Saya</h2>
-      </div>
-      <div className="bg-gray-200 p-20 relative z-0">
-        <div className="container mx-auto grid xl:grid-cols-4 md:grid-cols-2 justify-items-center">
-          {portofolio.map((item) => {
-            return (
-              <PortofolioCard
-                key={item.id}
-                link={
-                  'https://play-lh.googleusercontent.com/RslBy1o2NEBYUdRjQtUqLbN-ZM2hpks1mHPMiHMrpAuLqxeBPcFSAjo65nQHbTA53YYn'
-                }
-                name={item.name}
-              />
-            );
-          })}
+        <div className="bg-gradient-to-r from-gray-400 to-gray-500 p-20 relative z-0">
+          <div className="container mx-auto grid xl:grid-cols-4 md:grid-cols-2 justify-items-center">
+            {portofolio.map((item) => {
+              return (
+                <PortofolioCard
+                  key={item.id}
+                  link={
+                    'https://play-lh.googleusercontent.com/RslBy1o2NEBYUdRjQtUqLbN-ZM2hpks1mHPMiHMrpAuLqxeBPcFSAjo65nQHbTA53YYn'
+                  }
+                  name={item.name}
+                />
+              );
+            })}
+          </div>
         </div>
-      </div>
+      </section>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto max-w-7x1">
           <div className="flex flex-wrap w-full mb-4 p-4">
@@ -88,20 +103,20 @@ const Home = () => {
             {berita.slice(0, 6).map((item) => {
               return (
                 <div key={item.postid} className="xl:w-1/3 md:w-1/2 p-4">
-                  <div className="bg-gray-200 p-6 rounded-lg">
+                  <div className="bg-gray-300 p-6 rounded-lg shadow-lg">
                     <img
                       className="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6"
                       src={item.gambar}
                       alt={item.judul}
                     />
-                    <h3 className="tracking-widest text-gray-500 text-xs font-medium title-font">
+                    <h3 className="tracking-widest text-black text-xs font-medium title-font">
                       {item.tanggal}
                     </h3>
-                    <h2 className="text-lg text-gray-600 font-medium title-font mb-4">
+                    <h2 className="text-lg text-black font-medium title-font mb-4">
                       {item.judul}
                     </h2>
                     <p className="leading-relaxed text-base">
-                      {item.konten.substring(0, 250)}
+                      {item.konten.substring(0, 200)}
                     </p>
                   </div>
                 </div>
@@ -109,6 +124,31 @@ const Home = () => {
             })}
           </div>
         </div>
+      </section>
+      <section className="">
+        <Slider {...settings}>
+          <div className="hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div className="flex flex-col justify-between p-4 leading-normal">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Laravel
+              </h5>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                Telah membuat beberapa aplikasi sejak tahun
+              </p>
+            </div>
+          </div>
+          <div className="hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div className="flex flex-col justify-between p-4 leading-normal">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+            </div>
+          </div>
+        </Slider>
       </section>
     </Fragment>
   );
